46df5fa5e3f5516993efc7083df0e1a0d3329b75
Dianitha <diana.rodriguezprofa@gmail.com>
2024-09-05 00:51:14 -0500

    commit de proyecto compilado 7

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
-import NoPages from "./Pages/NoPages";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="productos" element={<Product />} />
-          <Route path="*" element={<NoPages />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
